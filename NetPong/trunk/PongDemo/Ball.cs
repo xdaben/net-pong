7a7bf68a24d70f9b2adf24f38d3c2ebdfb978ed4
mammalpride@gmail.com <mammalpride@gmail.com@f28dc93b-b4ca-9552-e1cd-36c4e746314f>
2012-03-23 23:59:09 +0000

    Basic Pong Demo.  Only 1 paddle is controlled, no score keeping, etc.

---
+++ NetPong/trunk/PongDemo/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+
+namespace PongDemo
+{
+    class Ball
+    {
+        // private fields
+        private double angle;
+        private double speed;
+
+        // properties
+        public Rectangle Sprite { get; set; }
+        public double XPos { get; set; }
+        public double YPos { get; set; }
+        public int LastPaddle { get; set; }
+
+        public double Angle
+        {
+            get { return angle; }
+            set { angle = value % 360; }
+        }
+
+        public double Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public Ball()
+        {
+            Angle = 0;
+            Speed = 1;
+            XPos = 0;
+            YPos = 0;
+        }
+    }
+}
