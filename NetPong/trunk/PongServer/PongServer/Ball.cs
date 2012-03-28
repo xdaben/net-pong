67096de0fe9f1f76cfb89ae7ee4a1658340b15c5
mastaman@gmail.com <mastaman@gmail.com@f28dc93b-b4ca-9552-e1cd-36c4e746314f>
2012-03-28 06:29:09 +0000

    More server stuff. Created classes for all the objects in the game.

---
+++ NetPong/trunk/PongServer/PongServer/Ball.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PongServer
+{
+    class Ball
+    {
+        public double Xpos { get; set; }
+        public double Ypos { get; set; }
+        public int Angle { get; set; }
+        public int Speed { get; set; }
+    }
+}
---
+++ NetPong/trunk/PongServer/PongServer/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace PongServer
+{
+    class Player
+    {
+        public double Xpos { get; set; }
+        public double Ypos { get; set; }
+        public int Score { get; set; }
+        public Socket NetPlayer { get; set; }
+    }
+}
