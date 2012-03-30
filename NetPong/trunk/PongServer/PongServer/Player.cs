3ef2b6682e88b15a6cf430f65a43f7f2af28c04f
mastaman@gmail.com <mastaman@gmail.com@f28dc93b-b4ca-9552-e1cd-36c4e746314f>
2012-03-30 05:36:35 +0000

    Begin Async socket update     Player Joins are now asynchronous(threaded) + dynamic(any amount of users could technically join)     If a player joins and the server is full (2 players max) it will send a message to that player that the server is full and close the connection

--- NetPong/trunk/PongServer/PongServer/Player.cs
+++ NetPong/trunk/PongServer/PongServer/Player.cs
@@ -8,9 +8,29 @@ namespace PongServer
 {
     class Player
     {
+        public int PlayerNum { get; set; }
         public double Xpos { get; set; }
         public double Ypos { get; set; }
         public int Score { get; set; }
-        public Socket NetPlayer { get; set; }
+        private StateObject state = new StateObject();
+        public StateObject State
+        {
+            get
+            {
+                return state;
+            }
+        }
+
+        public Socket NetPlayer
+        {
+            get
+            {
+                return State.workSocket;
+            }
+            set
+            {
+                State.workSocket = value;
+            }
+        }
     }
 }
