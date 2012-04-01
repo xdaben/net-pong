5d38b24327708d25a9fab3675489c7a48676e2c2
mammalpride@gmail.com <mammalpride@gmail.com@f28dc93b-b4ca-9552-e1cd-36c4e746314f>
2012-04-01 21:37:14 +0000

    This client contains none of the game logic.  It has methods to update the display elements based upon coordinates that will hopefully be received from the server.

---
+++ NetPong/trunk/PongClient/GameObjects.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PongClient
+{
+    class GameObjects
+    {
+        #region fields
+        private double ballXPos, ballYPos;
+        private double paddle1XPos, paddle1YPos;
+        private double paddle2XPos, paddle2YPos;
+        private double player1Score, player2Score;
+        #endregion
+
+        #region properties
+        public double BallXPos
+        {
+            get { return ballXPos; }
+            set { ballXPos = value; }
+        }
+
+        public double BallYPos
+        {
+            get { return ballYPos; }
+            set { ballYPos = value; }
+        }
+
+        public double Paddle1XPos
+        {
+            get { return paddle1XPos; }
+            set { paddle1XPos = value; }
+        }
+
+        public double Paddle1YPos
+        {
+            get { return paddle1YPos; }
+            set { paddle1YPos = value; }
+        }
+
+        public double Paddle2XPos
+        {
+            get { return paddle2XPos; }
+            set { paddle2XPos = value; }
+        }
+
+        public double Paddle2YPos
+        {
+            get { return paddle1YPos; }
+            set { paddle2YPos = value; }
+        }
+
+        public double Player1Score
+        {
+            get { return player1Score; }
+            set { player1Score = value; }
+        }
+
+        public double Player2Score
+        {
+            get { return player2Score; }
+            set { player2Score = value; }
+        }
+        #endregion
+
+    }
+}
---
+++ NetPong/trunk/PongClient/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Threading;
+
+namespace PongClient
+{
+    /// <summary>
+    /// doubleeraction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Rectangle paddle1Sprite, paddle2Sprite, ballSprite;
+        GameObjects game = new GameObjects();
+        Thread networkThread;
+        DispatcherTimer displayTimer;
+        int direction;
+        int playerNumber;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitSprites();
+            Update();
+        }
+
+        #region network initializers
+        private void ConnectToServer()
+        { 
+            /*
+             *  Okay, we need to connect to the server,
+             *  receive the assigned player number,
+             *  and initialize the game object coordinates.
+             */
+        }
+        #endregion
+
+        #region display initializers
+        private void InitSprites()
+        {
+            ballSprite = MakeBallSprite();
+            Canvas.SetLeft(ballSprite, 35);
+            Canvas.SetTop(ballSprite, 180);
+
+            paddle1Sprite = MakePaddleSprite();
+            Canvas.SetLeft(paddle1Sprite, 15);
+            Canvas.SetTop(paddle1Sprite, 150);
+
+            paddle2Sprite = MakePaddleSprite();
+            Canvas.SetLeft(paddle2Sprite, 560);
+            Canvas.SetTop(paddle2Sprite, 150);
+
+            // these assignments are temporary, just to make
+            // the UI look pretty until the client-server stuff
+            // is done
+            game.BallXPos = Canvas.GetLeft(ballSprite);
+            game.BallYPos = Canvas.GetTop(ballSprite);
+
+            game.Paddle1XPos = Canvas.GetLeft(paddle1Sprite);
+            game.Paddle1YPos = Canvas.GetTop(paddle1Sprite);
+
+            game.Paddle2XPos = Canvas.GetLeft(paddle2Sprite);
+            game.Paddle2YPos = Canvas.GetTop(paddle2Sprite);
+        }
+
+        private Rectangle MakePaddleSprite()
+        {
+            Rectangle sprite = new Rectangle();
+            sprite.Width = 20;
+            sprite.Height = 80;
+            sprite.Fill = Brushes.Green;
+            GameCourt.Children.Add(sprite);
+            return sprite;
+        }
+
+        private Rectangle MakeBallSprite()
+        {
+            Rectangle sprite = new Rectangle();
+            sprite.Width = 20;
+            sprite.Height = 20;
+            sprite.Fill = Brushes.White;
+            GameCourt.Children.Add(sprite);
+            return sprite;
+        }
+        #endregion
+
+        private void Update()
+        {
+            networkThread = new Thread(new ThreadStart(NetworkUpdate));
+            networkThread.Start(); 
+            
+            displayTimer = new DispatcherTimer();
+            displayTimer.Interval = new TimeSpan(0, 0, 0, 0, 15);
+            displayTimer.Tick += DisplayTimerHandler;
+            displayTimer.Start();
+        }
+
+        private void NetworkUpdate()
+        {
+            while (true)
+            {
+                /*
+                 *  Here we need to tell the server which
+                 *  direction the client's paddle is moving,
+                 *  and receive the new coordinates and updated
+                 *  scores.
+                 */
+                
+                Thread.Sleep(1); // we can tweak this value if the buffer overflows
+            }
+        }
+
+        private void DisplayTimerHandler(object sender, EventArgs e)
+        {
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            //update ball
+            Canvas.SetLeft(ballSprite, game.BallXPos);
+            Canvas.SetTop(ballSprite, game.BallYPos);
+
+            // udpate paddle1
+            Canvas.SetLeft(paddle1Sprite, game.Paddle1XPos);
+            Canvas.SetTop(paddle1Sprite, game.Paddle1YPos);
+
+            // update paddle2
+            Canvas.SetLeft(paddle2Sprite, game.Paddle2XPos);
+            Canvas.SetTop(paddle2Sprite, game.Paddle2YPos);
+
+            // update score labels
+            playerOneScoreLabel.Content = game.Player1Score;
+            playerTwoScoreLabel.Content = game.Player2Score;
+        }
+
+        #region user input
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Up)
+                direction = -1;
+            if (e.Key == Key.Down)
+                direction = 1;
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Up || e.Key == Key.Down)
+                direction = 0;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            networkThread.Abort();
+        }
+        #endregion
+    }
+}
